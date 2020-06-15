f75f09754fbfc4a367b6a1abf7cf42039a6470cd
Zion Taber <62846363+zmt28@users.noreply.github.com>
2020-06-15 14:58:18 -0400

    Update Streamlit_ML.py

--- Streamlit_ML.py
+++ Streamlit_ML.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from sklearn import datasets
+
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import Normalizer
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,6 +19,7 @@ dataset_name = st.sidebar.selectbox('Select Dataset', ('Iris', 'Breast Cancer', 
 classifier_name = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'Random Forest'))
 
 
+# load the chosen Data
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
@@ -37,29 +41,43 @@ def add_parameter_ui(clf_name):
     params = dict()
     if clf_name == "KNN":
         K = st.sidebar.slider("K", 1, 15)
+        P = st.sidebar.slider("P", 1.0, 5.0)
         params["K"] = K
+        params["P"] = P
     elif clf_name == "SVM":
         C = st.sidebar.slider("C", 0.01, 10.0)
         params["C"] = C
+        G = st.sidebar.slider("Gamma", 1, 20)
+        params["G"] = G
     else:
         max_depth = st.sidebar.slider("max_depth", 2, 15)
         n_estimators = st.sidebar.slider("n_estimators", 1, 100)
+        min_samp_split = st.sidebar.slider("min_samples_split", .1, 1.0)
         params["max_depth"] = max_depth
         params["n_estimators"] = n_estimators
+        params["min_samp_split"] = min_samp_split
     return params
 
 
 params = add_parameter_ui(classifier_name)
+normalizer = Normalizer()
+if classifier_name == 'Random Forest':
+    normalized = False
+else:
+    normalized = True
 
 
 def get_classifier(clf_name, params):
     if clf_name == "KNN":
-        clf = KNeighborsClassifier(n_neighbors=params["K"])
+        clf = KNeighborsClassifier(n_neighbors=params["K"], p=params["P"])
+        normalizer.fit_transform(X=X)
     elif clf_name == "SVM":
-        clf = SVC(C=params["C"])
+        clf = SVC(C=params["C"], gamma=params["G"])
+        normalizer.fit_transform(X=X)
     else:
         clf = RandomForestClassifier(n_estimators=params["n_estimators"],
-                                     max_depth=params["max_depth"], random_state=17)
+                                     max_depth=params["max_depth"], min_samples_split=params["min_samp_split"],
+                                     random_state=17)
     return clf
 
 
@@ -73,6 +91,7 @@ y_pred = clf.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 st.write(f"classifier = {classifier_name}")
 st.write(f"accuracy = {acc}")
+st.write(f"normalized? = {normalized}")
 
 # PLOT
 pca = PCA(2)
@@ -90,8 +109,6 @@ plt.colorbar()
 # plt show
 st.pyplot()
 
-
 # TODO
 # add more parameters to models
 # add more/other classifiers
-# add feature scaling
